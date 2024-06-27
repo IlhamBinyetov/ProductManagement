893e209318199b01d0bd6d91426169c02d07cad5
Ilham Binyetov <ilhamgb@code.edu.az>
2024-06-27 17:07:00 +0400

    Create new models and Controller

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProductManagement.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=ProductManagement.db");
+            base.OnConfiguring(optionsBuilder);
+
+        }
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductManagement.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public Customer Customer { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
